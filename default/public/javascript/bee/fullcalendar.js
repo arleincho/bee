eba5124b9c9dc18728d3e4353416c66aa3b2af5d
JulianPena <julian3079@gmail.com>
2016-05-27 22:41:32 -0500

    ajustes julian

--- default/public/javascript/bee/fullcalendar.js
+++ default/public/javascript/bee/fullcalendar.js
@@ -5351,7 +5351,7 @@ DayGrid.mixin({
 						event.hour1+
 					'</span>'+
 					'<span class="descripEvento" style="line-height: 17px;font-size: 12px ! important; border-left: 1px solid #fff; text-align:left !important; font-size: 12px !important;letter-spacing: 0;">' + 
-						unescape(event.constraint)+
+						unescape(event.description)+
 					'</span>'+
 					'<span class="descripEvento" style="line-height: 17px;font-weight: 800;border-left: 1px solid;font-size: 12px ! important; text-align:left !important; font-size: 12px !important;letter-spacing: 0;">' + 
 						event.author+
